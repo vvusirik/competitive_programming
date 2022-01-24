0848a1ede2472c1193e1c74e78ec0166b119d7f5
Vishal Vusirikala <vvusirik@gmail.com>
2022-01-23 23:00:20 -0800

    counting rooms

--- counting_rooms/main.cpp
+++ counting_rooms/main.cpp
@@ -6,27 +6,21 @@ typedef unsigned long long int llu;
 int n, m;
 int h[] = {-1, 1, 0, 0};
 int v[] = {0, 0, -1, 1};
+char grid[1001][1001];
 
-void bfs(vector<vector<char>> &grid, int i, int j) {
-  queue<pair<int, int>> q;
-  q.emplace(i, j);
-  pair<int, int> p;
-  while (q.size() > 0) {
-    const auto &[i, j] = q.front();
-    q.pop();
-    grid[i][j] = 'x';
+bool valid(int x, int y) {
+  if (x < 0 || x >= n || y < 0 || y >= m)
+    return false;
+  return true;
+}
 
-    if (i - 1 >= 0 && grid[i - 1][j] == '.')
-      q.emplace(i - 1, j);
-
-    if (i + 1 < grid.size() && grid[i + 1][j] == '.')
-      q.emplace(i + 1, j);
-
-    if (j - 1 >= 0 && grid[i][j - 1] == '.')
-      q.emplace(i, j - 1);
-
-    if (j + 1 < grid[0].size() && grid[i][j + 1] == '.')
-      q.emplace(i, j + 1);
+void dfs(int i, int j) {
+  grid[i][j] = 'o';
+  for (int d = 0; d < 4; ++d) {
+    int dx = i + h[d];
+    int dy = j + v[d];
+    if (valid(dx, dy) && grid[dx][dy] == '.')
+      dfs(dx, dy);
   }
 }
 
@@ -35,14 +29,9 @@ int main() {
   cin.tie(0);
 
   cin >> n >> m;
-  vector<vector<char>> grid;
-  grid.resize(n);
-  char val;
   for (int i = 0; i < n; ++i) {
-    grid[i].resize(m);
     for (int j = 0; j < m; ++j) {
-      cin >> val;
-      grid[i][j] = val;
+      cin >> grid[i][j];
     }
   }
 
@@ -50,8 +39,7 @@ int main() {
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
       if (grid[i][j] == '.') {
-        cout << i << " " << j;
-        bfs(grid, i, j);
+        dfs(i, j);
         rooms++;
       }
     }
